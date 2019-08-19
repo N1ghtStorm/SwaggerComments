a7cd4a23809a9034f530ef9aeb4916cdaec117d6
unknown <bushuevdv@mail.ru>
2019-08-19 22:07:27 +0300

    first commit

---
+++ SwaggerCommentsMaker/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SwaggerCommentsMaker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.RichTextBox();
+            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
+            this.TransferButton = new System.Windows.Forms.Button();
+            this.CleanButton = new System.Windows.Forms.Button();
+            this.PhraseTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.InputTextBox.Location = new System.Drawing.Point(43, 79);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(397, 404);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.Text = "";
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OutputTextBox.Location = new System.Drawing.Point(712, 79);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(467, 404);
+            this.OutputTextBox.TabIndex = 1;
+            this.OutputTextBox.Text = "";
+            // 
+            // TransferButton
+            // 
+            this.TransferButton.Location = new System.Drawing.Point(504, 518);
+            this.TransferButton.Name = "TransferButton";
+            this.TransferButton.Size = new System.Drawing.Size(133, 46);
+            this.TransferButton.TabIndex = 2;
+            this.TransferButton.Text = "Перевести";
+            this.TransferButton.UseVisualStyleBackColor = true;
+            this.TransferButton.Click += new System.EventHandler(this.TransferButton_Click);
+            // 
+            // CleanButton
+            // 
+            this.CleanButton.Location = new System.Drawing.Point(1071, 547);
+            this.CleanButton.Name = "CleanButton";
+            this.CleanButton.Size = new System.Drawing.Size(117, 46);
+            this.CleanButton.TabIndex = 3;
+            this.CleanButton.Text = "Очистить";
+            this.CleanButton.UseVisualStyleBackColor = true;
+            this.CleanButton.Click += new System.EventHandler(this.CleanButton_Click);
+            // 
+            // PhraseTextBox
+            // 
+            this.PhraseTextBox.Location = new System.Drawing.Point(522, 263);
+            this.PhraseTextBox.Name = "PhraseTextBox";
+            this.PhraseTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PhraseTextBox.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1216, 621);
+            this.Controls.Add(this.PhraseTextBox);
+            this.Controls.Add(this.CleanButton);
+            this.Controls.Add(this.TransferButton);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "SwaggerComments";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox InputTextBox;
+        private System.Windows.Forms.RichTextBox OutputTextBox;
+        private System.Windows.Forms.Button TransferButton;
+        private System.Windows.Forms.Button CleanButton;
+        private System.Windows.Forms.TextBox PhraseTextBox;
+    }
+}
+
---
+++ SwaggerCommentsMaker/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwaggerCommentsMaker
+{
+    public partial class MainForm : Form
+    {
+        private readonly string _defaultPhrase;
+        public MainForm()
+        {
+            InitializeComponent();
+            _defaultPhrase = "/// ";
+        }
+
+
+        private void TransferButton_Click(object sender, EventArgs e)
+        {
+            var count = InputTextBox.Text.Count(Char.IsDigit);
+            string[] TextToTransfer = new string[count];
+
+            OutputTextBox.Clear();
+
+            using (StringReader reader = new StringReader(InputTextBox.Text))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Do something with the line
+                    line = _defaultPhrase + line;
+
+                    if (!string.IsNullOrWhiteSpace(OutputTextBox.Text))
+                    {
+                        OutputTextBox.AppendText("\r\n" + line);
+                    }
+                    else
+                    {
+                        OutputTextBox.AppendText(line);
+                    }                     
+                    OutputTextBox.ScrollToCaret();
+                }
+            }
+        }
+
+        private void CleanButton_Click(object sender, EventArgs e)
+        {
+            InputTextBox.Clear();
+            OutputTextBox.Clear();
+        }
+    }
+}
